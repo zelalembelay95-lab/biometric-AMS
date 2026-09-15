f4aca303b9fd3a50720af83d1f5d03e9191157d3
zelalembelay95-lab <zelalembelay95@gmail.com>
2026-09-15 12:03:11 +0300

    Update vite.config.js

--- vite.config.js
+++ vite.config.js
@@ -1,13 +1,6 @@
 import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react'
 
-// https://vitejs.dev
 export default defineConfig({
-  plugins: [react()],
-  build: {
-    rollupOptions: {
-      // Tells Vite to ignore face-api.js during compilation since it's loaded via CDN
-      external: ['face-api.js'],
-    },
-  },
+  plugins: [react()]
 })
